2ea2bbd10e9527f78c8c98682df019e8dce17a47
Emma Junes <emmaastrom91@gmail.com>
2022-09-27 09:59:53 +0200

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Donkey</title>
+        <link rel="stylesheet" href="style.css">
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+
+    </head>
+   <body>
+    <div class="hero-image">
+      <div class="wrapper">
+      <nav class="nav">
+          <img src="/images/Logo.svg">
+        <ul>
+          <li>Våra kläder</li>
+          <li>Outlet</li>
+          <li>Butiker</li>
+          <li>Kundservice</li>
+          <li>Om oss</li>
+        </ul>
+        <button class="nav-button">Get in touch</button>
+      </nav>
+      <header class="main-head">
+        <h1>Så kliv in i Donkeys magiska värld - där barn alltid är barn.</h1>
+        <p class="header-text">Donkey har kläder till barn, från nyfödd och baby, till barn i förskole- och skolåldern. Barnkläder och bebiskläder som underlättar vardagen och som fungerar lika bra 
+          för fartfyllda utomhusaktiviteter som till fest.</p>
+          <button class="header-button">Våra kläder</button>
+    </header>
+      </div>
+    </div>
+
+    <div class="wrapper"> 
+        <div class="news">
+          <div class="left-column">
+            <p class="small-title">HÖSTKLÄDER</p>
+            <h2>Härliga höstnyheter</h2>
+            <p class="news-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at sem arcu. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. 
+            </p>
+            <button class="shop-button">Shoppa nu</button>
+          </div>
+          <img src="/images/IMG.png" class="news-image img1">
+          <img src="/images/IMG (1).png" class="news-image img2">
+        <div class="right-column">
+          <p class="small-title">VÅRT SORTIMENT</p>
+          <h2>Hållbara plagg</h2>
+          <p class="news-text">Med 100 % hållbarhetsmärkt bomull i hela vår kollektion gör vi kläder till baby och barn, men också kläder till den nyfödda familjemedlemmen.</p>
+          <button class="readmore-button">Läs mer</button>
+        </div>
+      </div>
+        <div class="background-form">
+          <div class="contact">
+            <h3>STÄLL EN FRÅGA</h3>
+            <h2>Har du något du funderar över?</h2>
+          <p class="form-textbox">Med 100 % hållbarhetsmärkt bomull i hela vår kollektion gör vi kläder till baby och barn, men också kläder till den nyfödda familjemedlemmen.</p>
+
+          <form class="form">
+            <p class="small-title-form">Namn <input class="user-info" name="namn" value="Skriv ditt namn"></p>
+            <p class="small-title-form">Email <input class="user-info" type="email" name="email" value="Fyll i din mailadress"></p>
+              <div class="category-block">
+              <div class="title">
+              <p class="small-title-form">Välj underkategori</p>
+              </div>
+              <div class="radio">
+                <label>
+                <input class="category-input" type="radio" name="category" value="category1" id="category1"/>
+              <div class="category-div">Kategori 1</div>
+                </label>
+                <label>
+                <input class="category-input" type="radio" name="category" value="category2" id="category2"/>
+              <div class="category-div">Kategori 2</div>
+                </label>
+                <label>
+                <input class="category-input" type="radio" name="category" value="category3" id="category3"/>
+              <div class="category-div">Kategori 3</div>
+                </label>
+                <label>
+                <input class="category-input" type="radio" name="category" value="category4"id="category4" />
+              <div class="category-div">Kategori 4</div>
+                </label>
+            </div>
+              </div>
+            <div class="form-buttons">
+              <button class="clear-button" type="submit">Rensa alternativ</button>
+              <button class="send-button" type="submit">Skicka</button>
+            </div>
+        </form>
+      </div>
+    </div>
+  </div>  
+    <div class="last-page">
+        <div class="img-footer">
+          <h2 class="final-title">Förändringen börjar nu!</h2>
+          <p class="white-text">Vi kommer alltid att utgå ifrån barnens bästa och vår hållbarhetsvision är att göra barnklädesmarknaden mer hållbar – för jorden går i arv, och det ska våra kläder också göra.</p>
+            <button class="last-button" type="submit">Vårt hållbarhetslöfte</button>
+        </div>
+     
+    <div class="background-footer">
+          <div class="footer-text">
+            <img src="/images/Logo (1).svg">
+            <div class="text-container">
+            <h2 class="footer-title">Donkey Clothing</h2>
+            <p>Donkey har kläder till barn, från nyfödd och baby, till barn i förskole- och skolåldern. Barnkläder och bebiskläder som underlättar vardagen och som fungerar lika bra för fartfyllda utomhusaktiviteter som till fest. Med 100 % hållbarhetsmärkt bomull i hela vår kollektion gör vi kläder till baby och barn, men också kläder till den nyfödda familjemedlemmen. Helt enkelt kläder som låter barn vara barn.</p>
+          </div>
+            <div class="contact-info">
+              <p>Donkey Clothing <br>194 61 Stockholm <br>Sweden</p>
+              <hr>
+              <p>+46 0 000 000 00</p>
+              <hr>
+              <p>info@donkey.com</p>
+          </div>
+        </div>  
+          <footer> 
+            <hr class="footer-hr">
+          <ul class="footer-meny">
+            <li>Om Donkey</li>
+            <li>Hitta våra butiker</li>
+            <li>Jobba hos oss</li>
+            <li>Press</li>
+            <li>Privacy Policy</li>
+            <li>Cookie Policy</li>
+          </ul>
+        </footer>
+      </div>
+            
+    </body>
+</html>
+
